6dffb2b95a2a412fe75b319ff7acf566c46c7616
samet.yildiz <sametyildiz03@gmail.com>
2025-08-22 16:28:37 +0300

    Initial gateway server

---
+++ gatewayserver/src/main/java/com/blogapp/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,49 @@
+package com.blogapp.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p -> p
+                        .path("/blogapp/account/serviceInfo")
+                        .filters(f -> f.setPath("/serviceInfo"))
+                        .uri("lb://ACCOUNT"))
+                .route(p -> p
+                        .path("/blogapp/account", "/blogapp/account/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://ACCOUNT"))
+
+                .route(p -> p
+                        .path("/blogapp/post/serviceInfo")
+                        .filters(f -> f.setPath("/serviceInfo"))
+                        .uri("lb://POST"))
+                .route(p -> p
+                        .path("/blogapp/post/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://POST"))
+
+                .route(p -> p
+                        .path("/blogapp/comment/serviceInfo")
+                        .filters(f -> f.setPath("/serviceInfo"))
+                        .uri("lb://COMMENT"))
+                .route(p -> p
+                        .path("/blogapp/comment", "/blogapp/comment/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://COMMENT"))
+                .build();
+    }
+}

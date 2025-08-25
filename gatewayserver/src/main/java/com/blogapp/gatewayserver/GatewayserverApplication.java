793847b635f3096a850bb48a94a9c750d80122e7
samet.yildiz <sametyildiz03@gmail.com>
2025-08-25 17:00:12 +0300

    Implemented circuit breaker on routing serviceInfo endpoints

--- gatewayserver/src/main/java/com/blogapp/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/blogapp/gatewayserver/GatewayserverApplication.java
@@ -6,21 +6,22 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
                         .path("/blogapp/account/serviceInfo")
-                        .filters(f -> f.setPath("/serviceInfo"))
+                        .filters(f -> f.setPath("/serviceInfo")
+                                .circuitBreaker(config ->
+                                        config.setName("accountInfoCircuitBreaker")
+                                                .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://ACCOUNT"))
                 .route(p -> p
                         .path("/blogapp/account", "/blogapp/account/**")
@@ -29,7 +30,10 @@ public class GatewayserverApplication {
 
                 .route(p -> p
                         .path("/blogapp/post/serviceInfo")
-                        .filters(f -> f.setPath("/serviceInfo"))
+                        .filters(f -> f.setPath("/serviceInfo")
+                                .circuitBreaker(config ->
+                                        config.setName("accountInfoCircuitBreaker")
+                                                .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://POST"))
                 .route(p -> p
                         .path("/blogapp/post/**")
@@ -38,7 +42,10 @@ public class GatewayserverApplication {
 
                 .route(p -> p
                         .path("/blogapp/comment/serviceInfo")
-                        .filters(f -> f.setPath("/serviceInfo"))
+                        .filters(f -> f.setPath("/serviceInfo")
+                                .circuitBreaker(config ->
+                                        config.setName("accountInfoCircuitBreaker")
+                                                .setFallbackUri("forward:/contactSupport")))
                         .uri("lb://COMMENT"))
                 .route(p -> p
                         .path("/blogapp/comment", "/blogapp/comment/**")

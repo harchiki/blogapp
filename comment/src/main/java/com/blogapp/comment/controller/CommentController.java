38ebc31f30a7a3db413fd7d446c0c59a8a0cf0d2
samet.yildiz <sametyildiz03@gmail.com>
2025-08-26 15:42:40 +0300

    Implemented retry mechanism on a dummy endpoint for practice

--- comment/src/main/java/com/blogapp/comment/controller/CommentController.java
+++ comment/src/main/java/com/blogapp/comment/controller/CommentController.java
@@ -2,7 +2,10 @@ package com.blogapp.comment.controller;
 
 import com.blogapp.comment.dto.CommentDto;
 import com.blogapp.comment.service.CommentService;
+import com.blogapp.exception.RetryException;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/comment")
 @RequiredArgsConstructor
+@Slf4j
 public class CommentController {
     private final CommentService commentService;
 
@@ -49,5 +53,17 @@ public class CommentController {
     public ResponseEntity<Void> deleteComment(@PathVariable("id") Long id) {
         commentService.delete(id);
         return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping(path = "/version")
+    @Retry(name = "getVersion", fallbackMethod = "getVersionFallback")
+    public ResponseEntity<String> getVersion() {
+        log.warn("getVersion invoked.");
+        throw new RetryException ("Retry attempt");
+    }
+
+    private ResponseEntity<String> getVersionFallback(Throwable t) {
+        return ResponseEntity.internalServerError().body("Service not available, try it again later");
     }
 }
--- comment/src/main/java/com/blogapp/comment/controller/ServiceInfoController.java
+++ comment/src/main/java/com/blogapp/comment/controller/ServiceInfoController.java
@@ -2,6 +2,7 @@ package com.blogapp.comment.controller;
 
 import com.blogapp.comment.dto.ServiceInfoDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/serviceInfo")
 @RequiredArgsConstructor
+@Slf4j
 public class ServiceInfoController {
     private final ServiceInfoDto serviceInfoDto;
 
     @GetMapping
-    public ResponseEntity<ServiceInfoDto> findPostById() {
-        return ResponseEntity.ok(serviceInfoDto);
+    public ResponseEntity<ServiceInfoDto> fetchServiceInfo() {
+        log.warn("fetchServiceInfo invoked.");
+        throw new RuntimeException("Retry attempt");
+        //return ResponseEntity.ok(serviceInfoDto);
     }
 }
